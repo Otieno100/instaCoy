e28d04b4fa8fc2d1d5b8400b40c792a11b4e8bd5
Otieno100 <brian.masafu@student.moringaschool.com>
2022-06-05 13:16:30 +0300

    created my first test

--- insta/tests.py
+++ insta/tests.py
@@ -1,3 +1,21 @@
+# from django.test import TestCase
+from unicodedata import name
 from django.test import TestCase
+from .models import Profile, Image, comments, likes
 
-# Create your tests here.
+
+class ProfileTestClass(TestCase):
+     """
+    defining testcase for User class
+    """
+     def setUp(self):
+         self.brian = Profile(name)
+
+     def test_instance(self) :
+         self.assertTrue(isinstance(self.brian,Profile)) 
+
+     
+     def test_save_method(self):
+        self.brian.save_profile()
+        Profiles = Profile.objects.all()
+        self.assertTrue(len(Profiles) > 0)   
