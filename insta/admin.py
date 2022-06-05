827c9405b8cb49eb11d6f69d5ecd766b180888d9
Otieno100 <brian.masafu@student.moringaschool.com>
2022-06-05 13:20:51 +0300

    added untracked files from migrations and venv

--- insta/admin.py
+++ insta/admin.py
@@ -1,9 +1,1 @@
-from django.contrib import admin
-from django.urls import re_path,include
-
-urlpatterns = [
-    re_path(r'^admin/', admin.site.urls),
-    re_path(r'',include('insta.urls'))
-    
-]
 
