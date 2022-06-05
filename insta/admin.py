31c3673ab8d8d8560d89a501e824c29d591aa2c6
Otieno100 <brian.masafu@student.moringaschool.com>
2022-06-05 15:37:33 +0300

    added image field in the admin.py

--- insta/admin.py
+++ insta/admin.py
@@ -1,1 +1,8 @@
+from django.contrib import admin
+from .models import Images,comments,likes,Profile
+
+admin.site.register(Images)
+admin.site.register(likes)
+admin.site.register(comments)
+admin.site.register(Profile)
 
