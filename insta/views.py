ab9336ff9de1deafbe094b5ff00c5aaf262e6f8d
Otieno100 <brian.masafu@student.moringaschool.com>
2022-06-05 13:17:17 +0300

    added views to index route

--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http  import HttpResponse
+import datetime as dt
+# from .models import  Images
 
 # Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+
+
+def profile(request):
+    date = dt.date.today()
+    # profile = Images.profile()
+    return render(request, 'instagram/profile.html', {"date": date,"post":profile})
