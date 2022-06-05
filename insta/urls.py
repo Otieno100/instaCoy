16b396303fb2822bd91cc86100ed9c3822cb80d2
Otieno100 <brian.masafu@student.moringaschool.com>
2022-06-05 18:26:12 +0300

    added search url in the project

--- insta/urls.py
+++ insta/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns=[
     # re_path(r'^$',views.index,name = 'welcome'),
     re_path('^$',views.profile,name='profile'),
+    re_path(r'^search/', views.search_results, name='search_results'),
     ]
