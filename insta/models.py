742f8d04cc62032cd33fcd701e0f164747b71d0e
Otieno100 <brian.masafu@student.moringaschool.com>
2022-06-05 13:52:43 +0300

    added image model

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from cProfile import Profile
 
-# Create your models here.
+class Profile(models.Model) :
+    name = models.CharField(blank=True, max_length=120)
+    bio = models.CharField(max_length = 30)
+    profile_image = models.ImageField(upload_to = 'image/',default = 'brian')
+
+    def save_profile(self):
+        self.save()
+        
+    def delete_profile(self):
+        self.delete()
+    
+    def update_profile(self):
+        self.update()
+    
+    def __str__(self):
+        return self.name
+    class Meta:
+        ordering = ['name']    
+
+    def __str__(self):
+      return self.name
+
+class likes(models.Model):
+    name = models.CharField(max_length = 30)
+
+    def __str__(self) :
+        return self.name 
+
+
+
+class Images(models.Model):
+    instagram_image = models.ImageField(upload_to = 'image/',default = 'brian')
+    image_name = models.CharField(max_length = 60)
+    image_caption = models.TextField()
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE )
+    likes = models.ManyToManyField(likes)
+
+            
